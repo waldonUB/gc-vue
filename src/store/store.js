39b6410b16095a1d1fed965e7409c5bcbd231565
waldonUB <852727515@qq.com>
2019-05-06 23:56:46 +0800

    vuex的配置加了注释

--- src/store/store.js
+++ src/store/store.js
@@ -10,6 +10,12 @@ export default new Vuex.Store({
   mutations: {
     increment: state => state.count++,
     decrement: state => state.count--,
+    /**
+     * @param state vuex中的状态
+     * @param item 传进来的参数
+     * 其他组件可以通过$store.commit来直接操作mutations（同步的方法），
+     * 也可以通过$store.dispatch操作action（异步的方法），action再操作mutations
+     * */
     getUserInfo (state, item) {
       state.userInfo = item
     }
